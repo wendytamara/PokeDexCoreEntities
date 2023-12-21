7b00c10753838ac37ab2414c4771bdfe51416814
Wendy Tamara Quispe Quispe <wquispeq@LIM-C02GQ351Q05P.local>
2023-12-21 15:08:03 -0500

    feat(show-viewDetail):  FEMS-05 add entities PokemonAttack

--- CoreEntities/Classes/Pokemon.swift
+++ CoreEntities/Classes/Pokemon.swift
@@ -1,23 +1,26 @@
 
-
 public struct Pokemon: Codable {
     
     public let idComic: String
     public let name: String
     public let about: String
     public let img: String
-    
     public let resistant: Array<String>
     public let weaknesses: Array<String>
+    public  let type: Array<String>
+    public  let specialAtack: [PokemonAtack]
 
-    public init(idComic: String, name: String, about: String , img: String, resistant: Array<String>, weaknesses: Array<String>) {
+    public init(idComic: String, name: String, about: String , img: String, resistant: Array<String>, weaknesses: Array<String>, type: Array<String>, specialAtack: [PokemonAtack]
+    ) {
         self.idComic = idComic
         self.name = name
         self.about = about
         self.img = img
-        
         self.resistant = resistant
         self.weaknesses = weaknesses
+        self.type = type
+        self.specialAtack = specialAtack
+
     }
     
     public func tableItemFormat() -> String {
--- CoreEntities/Classes/PokemonAtack.swift
+++ CoreEntities/Classes/PokemonAtack.swift
@@ -5,4 +5,19 @@
 //  Created by Wendy Tamara Quispe Quispe on 20/12/23.
 //
 
-import Foundation
+public struct PokemonAtack: Codable {
+    
+    public let name: String
+    public let type: String
+    public let baseDemage: String
+    public let moveDurationSeg: String
+    
+    public init(name: String, type: String, baseDemage: String, moveDurationSeg: String) {
+        self.name = name
+        self.type = type
+        self.baseDemage = baseDemage
+        self.moveDurationSeg = moveDurationSeg
+    }
+    
+
+}
